63f963482c379b17e40830eb9a132cbc4ffcc8a3
Pat Sabpisal <ssabpisa@me.com>
2016-05-09 18:53:40 +0700

    PIN logging

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/callback', function(req, res) {
-	console.log(req.body);
+	console.log('called back', req.body);
 });
 
 module.exports = router;
