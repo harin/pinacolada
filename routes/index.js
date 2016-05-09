3b0ef2b8d75803a3625e8d2ec642bd24933bd2e2
Pat Sabpisal <ssabpisa@me.com>
2016-05-09 19:29:40 +0700

    Pinx

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
 
 /* GET home page. */
-
 router.post('/callback', function(req, res) {
 	console.log('called back', req.body.result);
 	console.log('called back 2', req.body.result[0]['content']);
-	res.send("OK");
+
+	var hello = "Hi I'm Pin. You said" + req.body.result[0]['content']['text'];
+	var frm = req.body.result[0].from;	
+
+	unirest.post('https://trialbot-api.line.me/v1/events')
+	.headers({ 'Content-Type' : 'application/json; charset=UTF-8', 
+		   'X-Line-ChannelID' : '1466986405',
+		   'X-Line-ChannelSecret': '49dfcac9951abf6c6027c078065c24ee',
+		   'X-Line-Trusted-User-With-ACL': 'u3f49485a12a13764c9b80d37b9525757'
+	})
+	.send({
+		  "to":[frm],
+	 	  "toChannel":1383378250,
+	  	  "eventType":"138311608800106203",
+	  	  "content":{
+		      "messages": [{
+			  "contentType": 1,
+			  "text": hello	
+		      }]
+		  }
+	})
+	.end(function(msg){
+		res.send("OK");
+	});
+
+
 });
 
 module.exports = router;
