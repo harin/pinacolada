c7864ba54bc126ee43e31cdddd0957f55c7c09bc
Pat Sabpisal <ssabpisa@me.com>
2016-05-09 19:02:50 +0700

    Ch

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.post('/callback', function(req, res) {
-	console.log('called back', req.body, res.body.content);
+	console.log('called back', req.body, req.body['content']);
 });
 
 module.exports = router;
