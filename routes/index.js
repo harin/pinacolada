ef298419e7c9810dfd8150cfb9c3fa91070fdbd7
Pat Sabpisal <ssabpisa@me.com>
2016-05-14 20:06:35 +0700

    fsm

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var bc = require('../lib/bc');
 var _ = require('lodash');
 var Unakul = require('../lib/unakul');
+
+
 Unakul.callback = function(err, sender, msg){
 	bc.sendText([sender], msg);
 };
@@ -13,17 +15,17 @@ router.get('/', function(req,res){
 
 /* GET home page. */
 router.post('/callback', function(req, res) {
+	
 	console.log('called back', req.body.result);
 	
 	for(var i = 0 ;i < req.body.result.length; i++){
 		var result = req.body.result[i];
 		var isLocation = _.has(result.content, 'location.latitude');
-		var sender = [result.content.from];
+		var fromMID = result.content.from;
 		
 		if(isLocation){
-			// var location = result.content.location;
+			var location = result.content.location;
 			// bc.sendText(sender, 'You sent me a location ' + location.latitude + ", " + location.longitude);
-			// 
 		}else{
 			//Plain Text
 			var text = result.content.text;
