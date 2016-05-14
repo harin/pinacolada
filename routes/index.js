d6fedff572269e6494e938c39d6595d159ed8e30
Pat Sabpisal <ssabpisa@me.com>
2016-05-14 16:51:39 +0700

    modlaurl

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var unirest = require('unirest');
+var bc = require('./lib/bc');
+var _ = require('lodash');
 
 router.get('/', function(req,res){
 	res.send('PIN is up.');
@@ -9,29 +10,25 @@ router.get('/', function(req,res){
 /* GET home page. */
 router.post('/callback', function(req, res) {
 	console.log('called back', req.body.result);
-	// console.log('called back 2', req.body.result[0]['content']);
-
-	var hello = "Hi I'm Pin. You said " + req.body.result[0]['content']['text'];
-	var frm = req.body.result[0].content.from;
 	
-	unirest.post('https://api.line.me/v1/events')
-	.headers({ 'Content-Type' : 'application/json; charset=UTF-8', 
-		'X-LINE-ChannelToken' : 'GVkmAjF35ITv/o+20viSyJ3llXhudOoQATN85TxReCihmYLI0TdcyAOoU+kmAXVZFI0U9XUE7NNBSf3dvvxommf6VRYr+LcZ06yMu5lL8EgvJzXgPp7lcmUKUa6mJR3EKloHD0ie/kZNvjxtSXRVrq18BSl7lGXPAT9HRw/DX2c='
-	})
-	.send({
-		  "to":[frm],
-	 	  "toChannel":1383378250,
-	  	  "eventType":"138311608800106203",
-	  	  "content":{
-			  "contentType": 1,
-			  "toType": 1,
-			  "text": hello	
-		  }
-	})
-	.end(function(msg){
-		res.send("OK");
+	
+	req.body.result.forEach(function(result){
+		var isLocation = _.has(result.content, 'location.latitude');
+		var sender = result.content.from;
+		
+		if(isLocation){
+			var location = result.content.location;
+			bc.sendText(sender, 'You sent me a location ' + location.latitude + ", " + location.longitude);
+		}else{
+			//Plain Text
+			var text = result.content.text;
+			bc.sendText(sender, 'You sent me ' + text);
+		}
+	
 	});
 
+	
+	
 
 });
 
